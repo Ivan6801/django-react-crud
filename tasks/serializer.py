6679a2701455b13c6af8a9254f6f87af49c72052
Ivan Gonzalez <igonzalez@grupoexcel.com>
2023-08-14 19:05:43 -0700

    fix(backend): Fix the url

--- tasks/serializer.py
+++ tasks/serializer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ("id", "title", "description", "done")
+        model = Task
+        # fields = ("id", "title", "description", "done")
+        fields = "__all__"
