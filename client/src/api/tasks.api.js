26d100d69bd5459fe8a6e6c9fcbad21d9627aad7
Ivan Gonzalez <hal308366@gmail.com>
2023-08-24 14:21:41 -0700

    feat(frontend): Actualizar tareas

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -4,9 +4,12 @@ const tasksApi = axios.create({
   baseURL: "http://localhost:8080/tasks/api/v1/tasks/",
 });
 
-export const getAllTasks = () =>
-  axios.get("http://localhost:8080/tasks/api/v1/tasks/");
+export const getAllTasks = () => tasksApi.get("/");
+
+export const getTask = (id) => tasksApi.get(`/${id}`);
 
 export const createTask = (task) => tasksApi.post("/", task);
+
+export const updateTask = (id, task) => tasksApi.put(`/${id}/`, task);
 
 export const deleteTask = (id) => tasksApi.delete(`/${id}`);
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,20 +1,53 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { createTask, deleteTask } from "../api/tasks.api";
+import { createTask, deleteTask, getTask, updateTask } from "../api/tasks.api";
 import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 export function TaskFormPage() {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm();
   const navigate = useNavigate();
   const params = useParams();
 
   const onSubmit = handleSubmit(async (data) => {
-    await createTask(data);
+    if (params.id) {
+      await updateTask(params.id, data);
+      toast.success("Task updated", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
+    } else {
+      await createTask(data);
+      toast.success("New Task Added", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
+    }
+
     navigate("/tasks");
   });
+
+  useEffect(() => {
+    async function loadTask() {
+      if (params.id) {
+        const { data } = await getTask(params.id);
+        setValue("title", data.title);
+        setValue("description", data.description);
+      }
+    }
+    loadTask();
+  }, []);
 
   return (
     <div className="max-w-xl mx-auto">
@@ -39,18 +72,28 @@ export function TaskFormPage() {
         </button>
 
         {params.id && (
+        <div className="flex justify-end">
           <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
             onClick={async () => {
               const accepted = window.confirm("Are you sure?");
               if (accepted) {
                 await deleteTask(params.id);
+                toast.success("Task Removed", {
+                  position: "bottom-right",
+                  style: {
+                    background: "#101010",
+                    color: "#fff",
+                  },
+                });
                 navigate("/tasks");
               }
             }}
           >
-            Delete
+            delete
           </button>
-        )}
+        </div>
+      )}
       </form>
     </div>
   );
